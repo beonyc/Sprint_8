cbfe64e326257377c9286fecdc67cc9d0ad69da1
Yura Sgrobov <yrik0091@gmail.com>
2023-07-26 16:34:03 +0300

    aluure things added

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+
 public class Account {
     private final String name;
 
     public Account(String name) {
         this.name = name;
     }
-
+@Step
+@Description("валидация строки длина больше, длина меньше 19 и есть пробел между именем и фамилией")
     public boolean checkNameToEmboss() {
         return name.trim().length() > 3 && name.trim().length() < 19 && name.trim().contains(" ");
     }
--- src/test/java/org/example/AccountTest.java
+++ src/test/java/org/example/AccountTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,25 +15,32 @@ public class AccountTest {
     Account account;
 
     @Test
-
+    @DisplayName("тест валиднйо строки")
+    @Description("првоерка строки на соблюдение всех требование")
     public void checkIfNameIsValidTest() {
         account = new Account(validName);
         assertTrue(account.checkNameToEmboss());
     }
 
     @Test
+    @DisplayName("тест строки у которой длина меньше 3")
+    @Description("првоерка строки длины меньше 3")
     public void checkIfNameIsInValidWhenLessThen3LengthTest() {
         account = new Account(inValidNameLessThen3Characters);
         assertFalse(account.checkNameToEmboss());
     }
 
     @Test
+    @DisplayName("тест строки у которой длина юольше 19")
+    @Description("првоерка строки длины больше 19")
     public void checkIfNameIsInValidWhenMoreThan19LengthTest() {
         account = new Account(inValidNameMoreThen19Characters);
         assertFalse(account.checkNameToEmboss());
     }
 
     @Test
+    @DisplayName("тест строки у которой нет пробела между именем и фамилией")
+    @Description("првоерка строки без пробела")
     public void checkIfNameIsInValidWithoutSpaceTest() {
         account = new Account(inValidNameWithoutSpace);
         assertFalse(account.checkNameToEmboss());
